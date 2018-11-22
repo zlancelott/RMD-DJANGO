1cf5199f93a5ea34592132a1d5422467220f941a
Joao Carlos Prata <johncarlosilver@gmail.com>
2018-11-22 16:52:43 -0300

    Correcao2

--- RMD/RMD/settings.py
+++ RMD/RMD/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://rmd-repository.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['rmd-repository.herokuapp.com/', 'localhost']
 
 # Application definition
 
