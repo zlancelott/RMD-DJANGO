6cc09f7711dea267101bc4346e7f31cb93c6c4d4
Joao Carlos Prata <johncarlosilver@gmail.com>
2018-11-22 21:45:39 -0300

    static

--- RMD/RMD/settings.py
+++ RMD/RMD/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'home'
