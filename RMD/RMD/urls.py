9359ca51004f17a323e03fcb95c0c122215d280b
Joao Carlos Prata <johncarlosilver@gmail.com>
2018-11-22 22:53:00 -0300

    change

--- RMD/RMD/urls.py
+++ RMD/RMD/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from home import urls as home_urls
 from disciplinas import urls as disciplinas_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
