64875c3958f8a5b75c55ca4c5f82aa5cc9c3030a
Joao Carlos Prata <johncarlosilver@gmail.com>
2018-12-19 21:59:16 -0300

    Novo

--- RMD/RMD/settings.py
+++ RMD/RMD/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['rmd-repository.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rmd-repository.herokuapp.com', 'repository-rmd.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
